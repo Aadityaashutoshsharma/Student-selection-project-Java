90dc36d873ac0851c6a8bddbb5686362fbacaa30
Aadityaashutoshsharma <aadityaashutoshsharma@gmail.com>
2023-02-22 02:33:59 +0530

    Initial code stub

---
+++ src/StudentSelection.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;   //importing scanner package for taking the input
+
+//driver class
+public class StudentSelection {
+    public static void main (String[] args) {
+        System.out.println("Enter the total number of Students: ");
+
+        Scanner input = new Scanner(System.in);
+        int numOfStudents = input.nextInt();
+
+        String[] student =
+    }
+}
